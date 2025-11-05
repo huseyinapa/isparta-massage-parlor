03019b2e669fbabe8273b19664d38a610919a726
huseyin <huso.apa32@gmail.com>
2025-11-05 15:57:57 +0300

    Remove unused ReviewsFeature import from Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import FAQFeatures from "./_features/FAQ";
 import HomeFeature from "./_features/Home";
-import ReviewsFeature from "./_features/Reviews";
 import MassageServices from "./_features/Services";
 
 export default function Home() {
